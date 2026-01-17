2c7d4d4c1fe1157eaabe47725b8dfff5284c0000
ArtsemAverkov <102361520+ArtsemAverkov@users.noreply.github.com>
2026-01-17 17:35:20 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+// Selenium
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.17.0")
+// TestNG
+    testImplementation("org.testng:testng:7.9.0")
+// WebDriverManager
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
